7bfa02f8e47d92c0795fe626877f8106c441a836
Sebastián Jiménez <sjimenezc@psl.com.co>
2017-04-26 13:33:20 -0500

    Se documenta

--- src/main/java/co/com/psl/programmingchallenge/PslProgramingChallenge2.java
+++ src/main/java/co/com/psl/programmingchallenge/PslProgramingChallenge2.java
@@ -1,14 +1,16 @@
 package co.com.psl.programmingchallenge;
 
 public class PslProgramingChallenge2 {
-  public static void main(String[] args) {
-    double[] preciosAccion = {1100, 1500, 1200, 1300, 1700, 1600, 1400, 1500};
-    double[] preciosAccion1 = {2000, 1800, 1600, 1500, 1400, 1200};
-
-    System.out.println(calculateProfitability(preciosAccion));
-    System.out.println(calculateProfitability(preciosAccion1));
+  public static void main(double[] args) {
+    System.out.println(calculateProfitability(args));
   }
 
+  /**
+   * Calculates the total profitability of the given array of prices. <br>
+   * Prof = sales - purchases. 
+   * @param prices
+   * @return total profitability.
+   */
   public static double calculateProfitability(double[] prices) {
     double profitability = 0;
     for (int i = 0; i < prices.length; i++) {
